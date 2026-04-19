34d9dc735074c97c4cec3d6e1ca477fa478920d4
Jerry-Courage <jerry_2044@outlook.com>
2026-04-19 11:32:40 -0700

    FIX: Calibrate sys.path for Vercel monorepo

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the backend directory to sys.path so Vercel can find 'core'
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
